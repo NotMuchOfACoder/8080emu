874d6d6a6a188c61c6577ba6e705cd43a9dc9148
NotMuchOfACoder <88954932+NotMuchOfACoder@users.noreply.github.com>
2021-08-14 22:47:04 -0400

    Create assembly.c

---
+++ assembly.c
@@ -0,0 +1,5 @@
+   lea    a1, #$1000        ; the address of variable a    
+   lea    a2, #$1008        ; the address of variable b    
+   move.l d0,(a2)    
+   add.l  d0, #$64    
+   mov    (a1),d0
