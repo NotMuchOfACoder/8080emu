1323d1d6d9d4c93310cc8dc54eb4f0f2e193a093
NotMuchOfACoder <88954932+NotMuchOfACoder@users.noreply.github.com>
2021-08-14 22:50:19 -0400

    wrong copy and paste

--- stacks.c
+++ stacks.c
@@ -1,5 +1,7 @@
-   lea    a1, #$1000        ; the address of variable a    
+lea    a1, #$1000        ; the address of variable a    
    lea    a2, #$1008        ; the address of variable b    
+   move.l d0,(a2)    variable b    
    move.l d0,(a2)    
    add.l  d0, #$64    
-   mov    (a1),d0
+
+   add.l  d0, #$64    
