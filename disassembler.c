93ae59ce99372b43feaebb6661c8e1930cf7bb6f
NotMuchOfACoder <88954932+NotMuchOfACoder@users.noreply.github.com>
2021-08-14 22:54:50 -0400

    Update disassembler.c

--- disassembler.c
+++ disassembler.c
@@ -32,3 +32,30 @@
 
     return opbytes;    
    }    
+   int main (int argc, char**argv)    
+   {    
+    FILE *f= fopen(argv[1], "rb");    
+    if (f==NULL)    
+    {    
+        printf("error: Couldn't open %s\n", argv[1]);    
+        exit(1);    
+    }    
+
+    //Get the file size and read it into a memory buffer    
+    fseek(f, 0L, SEEK_END);    
+    int fsize = ftell(f);    
+    fseek(f, 0L, SEEK_SET);    
+
+    unsigned char *buffer=malloc(fsize);    
+
+    fread(buffer, fsize, 1, f);    
+    fclose(f);    
+
+    int pc = 0;    
+
+    while (pc < fsize)    
+    {    
+        pc += Disassemble8080Op(buffer, pc);    
+    }    
+    return 0;    
+   }    
