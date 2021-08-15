f2e27257b31d4500c083d7cbe4e6683678a5da42
NotMuchOfACoder <88954932+NotMuchOfACoder@users.noreply.github.com>
2021-08-14 22:52:28 -0400

    Update space_invaders.h

--- space_invaders.h
+++ space_invaders.h
@@ -9,3 +9,21 @@
    0000060 a7 c2 82 00 c3 65 07 3e 01 32 ea 20 c3 3f 00 cd    
    0000070 40 17 3a 32 20 32 80 20 cd 00 01 cd 48 02 cd 13    
    ...    
+   0000 00       NOP    
+   0001 00       NOP    
+   0002 00       NOP    
+   0003 c3 d4 18 JMP    $18d4    
+   0006 00       NOP    
+   0007 00       NOP    
+   0008 f5       PUSH   PSW    
+   0009 c5       PUSH   B    
+   000a d5       PUSH   D    
+   000b e5       PUSH   H    
+   000c c3 8c 00 JMP    $008c    
+   000f 00       NOP    
+   0010 f5       PUSH   PSW    
+   0011 c5       PUSH   B    
+   0012 d5       PUSH   D    
+   0013 e5       PUSH   H    
+   0014 3e 80    MVI    A,#0x80    
+   0016 32 72 20 STA    $2072    
